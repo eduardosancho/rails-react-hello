ac2f7667034f9ded8915a93075a5e04844bfa895
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-31 12:20:47 -0600

    Fix webpacker gem, display static#index in in root

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  /* eslint-disable global-require */
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
-        flexbox: 'no-2009',
+        flexbox: 'no-2009'
       },
-      stage: 3,
-    }),
-  ],
-  /* eslint-enable global-require */
-};
+      stage: 3
+    })
+  ]
+}
