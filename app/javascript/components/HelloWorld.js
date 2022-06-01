c5eac955ba70aded50137e4c77dabca0ab22a07f
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-31 20:49:03 -0600

    Add button to dispatch an action

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,16 +1,39 @@
 import React from "react"
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
 import PropTypes from "prop-types"
+
+const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST';
+
+function getThings() {
+  console.log('getThings() Action!!');
+  return {
+    type: GET_THINGS_REQUEST
+  };
+};
+
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+        <br />
+        <button className="getThingsBtn" onClick={() => this.props.getThings()}>getThings</button>
+
       </React.Fragment>
     );
   }
 }
 
+const structuredSelector = createStructuredSelector({
+  things: state => state.things,
+});
+
+const mapDispatchToProps = { getThings };
+
 HelloWorld.propTypes = {
   greeting: PropTypes.string
 };
-export default HelloWorld
+
+export default connect(structuredSelector, mapDispatchToProps)(HelloWorld);
