f48474abc8e637b93721cdd6b7be672176f34213
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-31 20:39:18 -0600

    Configure redux store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,20 +4,27 @@ import PropTypes from "prop-types";
 import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from "react-redux";
+
 import HelloWorld from "./HelloWorld";
+
+import configureStore from "../configureStore";
+const store = configureStore();
 
 class App extends React.Component {
   render () {
     return (
       <React.StrictMode>
-        <Router>
-          <Routes>
-            <Route path="/" element={"Hello World"} />
-            <Route path="/hello" element={ <HelloWorld greeting="Friend"/> } />
-          <React.Fragment>
-          </React.Fragment>
-          </Routes>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path="/" element={"Hello World"} />
+              <Route path="/hello" element={ <HelloWorld greeting="Friend"/> } />
+            <React.Fragment>
+            </React.Fragment>
+            </Routes>
+          </Router>
+        </Provider>
       </React.StrictMode>
     );
   }
